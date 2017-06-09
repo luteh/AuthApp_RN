ab0bbe4902dcc5402ccad357965fd854fe61cdf7
luteh <luthfanmaftuh@gmail.com>
2017-06-09 13:34:21 +0700

    Added password input and implemented secureTextEntry on TextInput of Input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,21 +5,29 @@ import React, { Component } from 'react'
 import { Card, CardSection, Button, Input } from './common'
 
 class LoginForm extends Component {
-  state = {email: ''}
+  state = {email: '', password: ''}
 
   render () {
     return (
       <Card>
         <CardSection>
           <Input
-            placeholder={"user@gmail.com"}
-            label = {"Email"}
+            placeholder={'user@gmail.com'}
+            label={'Email'}
             value={this.state.email}
             onChangeText={email => this.setState({email})}
           />
         </CardSection>
 
-        <CardSection/>
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder={'password'}
+            label={'Password'}
+            value={this.state.password}
+            onChangeText={password => this.setState({password})}
+          />
+        </CardSection>
 
         <CardSection>
           <Button>
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -4,12 +4,13 @@
 import React from 'react'
 import { View, Text, TextInput } from 'react-native'
 
-const Input = ({label, onChangeText, value, placeholder}) => {
+const Input = ({label, onChangeText, value, placeholder, secureTextEntry}) => {
   const {inputStyle, labelStyle, containerStyle} = styles
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
+        secureTextEntry={secureTextEntry}
         placeholder={placeholder}
         autoCorrect={false}
         style={inputStyle}
